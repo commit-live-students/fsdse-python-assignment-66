e7e7e2d99c44dba18dfc922e49b1f66f7699c113
Rohan2015 <rohandamodar@outlook.com>
2017-06-02 03:30:25 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,10 @@
 def solution(array):
-    """
-    Enter code here
-    """
+    if array == None:
+        return None
+
+    for i in range(0,len(array)):
+	    for j in range(0,len(array)):
+		    if i == j:
+			    if i == j == 1 or i == j == 2 or i == j == 3:
+				    array[i][j] = 0
+    return array
