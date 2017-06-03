161fe90157be1f9ae401c23d8f2643b4b447d84e
nikhilakki <nikhilakki@users.noreply.github.com>
2017-06-03 12:10:05 +0000

    Done

--- build.py
+++ build.py
@@ -4,9 +4,11 @@ def solution(n):
     """
     Enter code here
     """
-    mat1 = np.absolute(np.identity(n) - np.ones([n,n]))
+    m = n
+    mat1 = np.absolute(np.identity(5) - np.ones((5, 5)))
     mat1[0][0] = 1.0
     mat1[-1][-1] = 1.0
     return mat1
+
 
 print(solution(5))
