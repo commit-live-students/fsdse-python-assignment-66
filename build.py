8f2e567c9a847cf2c22f366037dcc16de552ccd8
nikhilakki <nikhilakki@users.noreply.github.com>
2017-06-03 12:01:53 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,12 @@
+import numpy as np
+
 def solution(array):
     """
     Enter code here
     """
+    mat1 = np.absolute(np.identity(5) - np.ones([5,5]))
+    mat1[0][0] = 1.0
+    mat1[-1][-1] = 1.0
+    return mat1
+
+print(solution(5))
