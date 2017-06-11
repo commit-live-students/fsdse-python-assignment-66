8a267cb703cc8b82a82e907bb0ae59f707c54502
coded9 <coded9@users.noreply.github.com>
2017-06-11 14:39:00 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,12 @@
-def solution(array):
+import numpy as np
+def solution(arr):
     """
     Enter code here
     """
+    shape = list(np.shape(arr))
+    a = shape[0]
+    b = shape[1]
+    for i in range(1,a):
+        arr[i,1:b-1] = 0
+    print arr
+    return arr
