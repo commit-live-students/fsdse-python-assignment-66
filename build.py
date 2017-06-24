c7cc0f21a4c4f4b02b2aa3a46bfc5713e441b567
adinathauti <adinathauti@gmail.com>
2017-06-24 06:16:55 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,13 @@
+import numpy as np
+
 def solution(array):
-    """
-    Enter code here
-    """
+    for i in range(1,4):
+        for j in range(1,4):
+            array[i][j] = 0.0
+    return array
+
+
+
+
+
+solution(np.ones((5,5)))
