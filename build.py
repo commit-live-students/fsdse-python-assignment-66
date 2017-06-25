cd4a9a13fbb3c141ecd8d6596ad44a97a16e2e69
saahil1292 <saahil1292@gmail.com>
2017-06-25 13:00:17 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,8 @@
+import numpy as np
+
 def solution(array):
-    """
-    Enter code here
-    """
+    input_matrix = np.array(array)
+    for num1 in range(1,4):
+        for num2 in range(1,4):
+            input_matrix[num1][num2] = 0
+    return input_matrix
