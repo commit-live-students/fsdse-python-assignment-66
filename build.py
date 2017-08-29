cd90403598d79828c2db9fc1c77d7e1144342015
SiddharthPant <spsiddharthpant@gmail.com>
2017-08-29 20:03:19 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,9 @@
+import numpy as np
+from copy import deepcopy
+
 def solution(array):
-    """
-    Enter code here
-    """
+    array = deepcopy(array)
+    m, n = array.shape
+    for i, row in enumerate(array[1 : m - 1]):
+        row[1: m - 1].fill(0.)
+    return array
