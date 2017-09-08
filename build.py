3ea3f8f17cef16b7fc3d8215fea27b12e4f4f553
rohanhg91 <rohanhgupta91@gmail.com>
2017-09-08 11:13:34 +0000

    Done

--- build.py
+++ build.py
@@ -2,3 +2,25 @@ def solution(array):
     """
     Enter code here
     """
+    for i in range (0, len(array)):
+        for j in range (0, len(array[i])):
+            if (i == 0 and j == 0) or (i > 0 and j == 0) or (i == 0 and j > 0) or (i == len(array)-1) or (j == len(array)-1):
+                array[i][j] = array[i][j]
+            else:
+                array[i][j] = 0
+
+    return array
+
+'''
+import numpy as np
+
+a = np.array([
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1]
+])
+
+print solution(a)
+'''
